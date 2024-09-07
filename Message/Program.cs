2a07c72f89c3d67e744d1c26ce0147cd04548487
Aaron Lal <lucidlear@gmail.com>
2024-09-07 14:46:32 +1000

    Add project files.

---
+++ Message/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Message
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IMessage message;
+
+            //Telegram telegram = new Telegram();
+            //SMS sms = new SMS();
+            //Email email = new Email();
+            //WhatsApp whatsApp = new WhatsApp();
+
+            IMessage message1 = new SMS();
+            IMessage message2 = new Email();
+            IMessage message3 = new WhatsApp();
+            IMessage message4 = new Telegram();
+
+            SendMessage(message4);
+        }
+
+        static void SendMessage(IMessage message)
+        {
+            message.SendMessage();
+        }
+    }
+}
---
+++ Message/SMS.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Message
+{
+    public class SMS : IMessage
+    {
+        public void SendMessage()
+        {
+            Console.WriteLine("sending SMS message");
+        }
+    }
+}
